5648d3e32168c6479b614750f8ada689c980d6d0
Ilja Spirts <69972734+TheCrabilia@users.noreply.github.com>
2023-03-31 14:22:02 +0000

    Add solution

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,40 @@
-# python3
+#!/usr/bin/env python3
 
-def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
-
-def print_occurrences(output):
-    # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
-
-def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
-
-    # and return an iterable variable
-    return [0]
+import sys
 
 
-# this part launches the functions
-if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+def rabin_karp(pattern: str, text: str) -> list[int]:
+    hpattern = hash(pattern)
+    for i in range(len(text) - len(pattern) + 1):
+        if hash(text[i : i + len(pattern)]) == hpattern:
+            if text[i : i + len(pattern)] == pattern:
+                yield i
 
+
+def main():
+    # I - input from keyboard
+    # F - input from file
+    mode = input("Input mode (I/F): ").strip().upper()
+    match mode:
+        case "I":
+            pattern = input("Pattern: ")
+            text = input("Text: ")
+        case "F":
+            filename = input("Filename: ")
+            if filename.endswith(".a"):
+                print("Invalid file name")
+                sys.exit(1)
+
+            with open(filename, "r") as f:
+                pattern = f.readline().strip()
+                text = f.readline().strip()
+        case _:
+            print("Invalid input mode")
+            sys.exit(1)
+
+    matches = rabin_karp(pattern, text)
+    print(" ".join(map(str, matches)))
+
+
+if __name__ == "__main__":
+    main()
